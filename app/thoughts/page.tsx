e97009f59e76c2baef6f8939d58df9e3c155c39b
Jaarabytes <xavierandole78@gmail.com>
2024-05-20 21:04:50 +0300

    feat: add basic routing
    add basic routes for app functioning. this app should be as minimal as possible.

---
+++ app/thoughts/page.tsx
@@ -0,0 +1,12 @@
+// This is the thoughts tab, don't get confused bozo
+
+export default function ThoughtsTab () {
+
+
+    return (
+        <>
+            <h1>#Thinking</h1>
+            <a></a>
+        </>
+    )
+}
