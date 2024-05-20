7f2cd66bfc2ffd07bbb901becd86c51ab61b80b6
Jaarabytes <xavierandole78@gmail.com>
2024-05-20 21:40:13 +0300

    feat: add structure
    add structure to posts tab and reminders

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,9 +1,39 @@
 // This is the posts tab, don't get confused bozo
 
 export default function PostsTab () {
+    const posts = [
+        {
+        id:1,
+        header: "Git rid of it",
+        path: "git-rid-of-it",
+        date: "14022024",
+        },
+        {
+        id:2,
+        header: "Minimal bash-like error",
+        path: "minimal-bash-like-error",
+        date: "03032024",
+        }
+      ]
     return (
         <>
             <h1>Posts: </h1>
+            <p>Some of these posts are problems i have encountered, so i put them here since i forget stuff</p>
+            <div>
+                {posts.reverse().map((element, index) => (
+                    <>
+                    <p>
+                    <span key={index}>{element.date}{index < posts.length && " "}</span>
+                    <a
+                    className="underline hover:text-red-400"
+                    href={`/${element.path}`}
+                    target="_blank"
+                    >{element.header}
+                    </a>
+                    </p>
+                    </>
+                ))}
+            </div>
         </>
     )
 }
