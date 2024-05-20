d954a9646976ccaeb44a7e15f366c0c5db721f24
Jaarabytes <xavierandole78@gmail.com>
2024-05-20 21:41:22 +0300

    feat: add more content
    my thoughts, my rules

--- app/thoughts/page.tsx
+++ app/thoughts/page.tsx
@@ -7,6 +7,18 @@ export default function ThoughtsTab () {
         <>
             <h1>#Thinking</h1>
             <a></a>
+            <p>I'll build an IQ test app, just need a first job</p>
+            <p>I like Bruce wayne, i think i want to be just like him</p>
+            <p>I would also like to work at <a
+            href="https://x.com/Pedroais2/status/1790776937947808045"
+            className="underline hover:text-red-400"
+            target="_blank"
+            >OpenZeppelin</a></p>
+            <p>How can I honestly say no to that?</p>
+            <p>You should never care about your wakatime, you should be worried about your portfolio</p>
+            {/* Provide links to dantotsu manga app */}
+            <p>You should read The Stranger</p>
+            <p>enjoy some manga too</p>
         </>
     )
 }
