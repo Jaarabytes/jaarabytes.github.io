2acba93f05b86fac856bb8279421ef7b9e7e147d
Jaarabytes <xavierandole78@gmail.com>
2024-05-21 11:58:08 +0300

    refactor: update structure
    add styling to the posts for responsiveness

--- app/posts/minimal-bash-like-error/page.tsx
+++ app/posts/minimal-bash-like-error/page.tsx
@@ -8,25 +8,29 @@ export default function MinimalBashLikeError () {
 
     return (
         <>
-            <div>
-                <Link href='/posts' className="underline hover:text-red-400">Posts</Link>
-                <h1>Minimal bash like error</h1>
-                <p>Have you ever encountered the below error (it occurs mostly on linux): </p>
+            <div className="mt-5 px-[20%]">
+                <Link href='/posts' className="underline text-red-400">Posts</Link>
+                <h1 className="text-xl font-bold my-5">Minimal bash like error</h1>
+                <p className="mb-5">Have you ever encountered the below error (it occurs mostly on linux): </p>
                 <i>
                 Minimal bash-like editing is supported. For the first word, TAB listing is possible with possible command completions
                 Anywhere else TAB is used, possible device or file completions are done. ESC anytime exists.
                 </i>
-                <img src="/minimal-bash-like-error/FY3tF38XEAAUiZF.jpeg" className="" alt="" />
+                <img src="/minimal-bash-like-error/FY3tF38XEAAUiZF.jpeg"
+                 className="my-5"
+                 width={600}
+                 height={400}
+                  alt="minimal bash like error message on laptop screen" />
                 <p>If you however use Windows, <a
                 href="https://youtu.be/hxOApe1P9dM?si=6YQ8UxzQBNvqPI5c"
                 className="underline hover:text-red-400"
                 target="_blank"
                 >click here</a></p>
-                <h2>Cause: </h2>
+                <h2 className="my-3 font-bold">Cause: </h2>
                 <p><b>Overwritten bootloader: </b>When two partitions exist (windows and linux) one's bootloader may overwrite GRUB </p>
                 <p><b>Accidental deletion: </b>You may have deleted grub configuration files eg <span className="text-red-400">`boot/grub/grub.cfg`</span> </p>
                 <p></p>
-                <h2>Solution: </h2>
+                <h2 className="my-3 font-bold">Solution: </h2>
                 <p><b>GRUB</b> GRand Unified Bootloader is responsible for showing you selection of Operating system after booting</p>
                 <h3>What to do?</h3>
                 <div>
@@ -36,7 +40,7 @@ export default function MinimalBashLikeError () {
                     </dl>
                 ))}
                 </div>
-                <p><b>Sidenote: </b> root variable partitions are where the entire operating system is installed and 
+                <p className="my-3"><b>Sidenote: </b> root variable partitions are where the entire operating system is installed and 
                 prefix variable partitions are where GRUB is installed.</p>
 
                 {/* Add code component here, Thank you, trenches */}
@@ -58,24 +62,26 @@ grub> insmod normal
 
 grub> normal`}>
                 </SyntaxHighlighter>
-                <img src="/minimal-bash-like-error/FY35CpFX0AI88iU.jpeg"></img>
-                <img src="/minimal-bash-like-error/FY35L0XX0AE-fdP.png"></img>
-                <img src="/minimal-bash-like-error/FY35R0fX0AASODJ.png"></img>
+                <img src="/minimal-bash-like-error/FY35CpFX0AI88iU.jpeg" className="my-5" width={550} height={300} alt="testing with (hd0)"></img>
+                <img src="/minimal-bash-like-error/FY35L0XX0AE-fdP.png" className="my-5" width={550} height={300} alt="testing with (hd0,gpt3)"></img>
+                <img src="/minimal-bash-like-error/FY35R0fX0AASODJ.png" className="my-5" width={550} height={300} alt="testing with (hd0,gpt2)"></img>
                 <p>After regaining access, reinstall and update you grub</p>
-                <p>It is stored in <span className="text-red-400">`/boot/grub`</span> for legacy BIOS and <span className="text-red-400">`boot/efi/grub`</span> or 
+                <p className="mb-5">It is stored in <span className="text-red-400">`/boot/grub`</span> for legacy BIOS and <span className="text-red-400">`boot/efi/grub`</span> or 
                 <span className="text-red-400">`/efi/grub`</span> for UEFI</p>
                 <img src="/minimal-bash-like-error/FY37XV2WYAIZmB3.jpeg"></img>
-                <p>Install G-parted or gnome disks</p>
+                <p className="my-5">Install G-parted or gnome disks</p>
                 <SyntaxHighlighter language='bash' style={dark} code={`sudo apt install gparted
 sudo apt install gnome-disk-utility`} />
-                <p>To reinstall and update grub, we run: </p>
+                <p className="my-5">To reinstall and update grub, we run: </p>
                 <SyntaxHighlighter language='bash' style={dark} code={`sudo grub-install /dev/sdXY
 #X is the disk number and Y is the partition number of EFI
 sudo update grub`} />
                 <img src="/minimal-bash-like-error/FY3-CiPX0AAwol1.jpeg"></img>
-                <p>Reboot your system and confirm if the error persists</p>
-                <p>Further reading: </p>
-                <div>
+                <p className="mt-5">Reboot your system and confirm if the error persists</p>
+                <p>Pictures used belong to twitter user <a className="text-red-400" 
+                href="https://twitter.com/known_mike" target="_blank">known_mike</a></p>
+                <p className="mt-5">Further reading: </p>
+                <div className="mb-5">
                     <a
                     className="text-red-400 hover:underline"
                     href="https://twitter.com/Known_Mike/status/1553180270815223809"
