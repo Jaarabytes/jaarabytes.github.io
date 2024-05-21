c3e26e0df0dcacf82ea3bf5504cc58e7ba00be3d
Jaarabytes <xavierandole78@gmail.com>
2024-05-21 11:55:29 +0300

    refactor: fix profile page
    change profile page to a better and simple look

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,42 @@
 import Image from "next/image"
-import Link from "next/link"
+import Navbar from "@/app/ui/Navbar"
+import AboutPage from "@/app/about/page"
 
 export default function Home () {
-  // I will map you out later
-  const relevantLinks = [{ name : "home", path : "/" }, { name : "posts", path : "/posts" },{ name : "thoughts",path : "/thoughts"},{ name : "about", path: "/about"}]
 
   return (
     <>
     {/* at posts, add naming git messages */}
     {/* Add a theme song (Hunter x hunter departure or the World deathnote) */}
     {/* add your about here */}
-      <div className="mt-5 text-center px-[20%]">
-        <Image
-        src="/kurapika.jpg"
-          alt="That's literally me (I'm oozing aura)"
-          width={200}
-          height={200}
-          className="items-center inline my-10"
-          style={{borderRadius: "50%"}}
-          />
-        <h1>Xavier</h1>
-        <p className="mb-5">(yes xavier is my real name)</p>
-        <p className="mb-5">Working on da blockchain</p>
-        <div className="block">
-          <a
-          className="hover:underline hover:text-red-400 block"
-          target="_blank"
-          href="https://twitter.com/jaarabytes"
-          >bird app ?</a>
-          <a
-          className="hover:underline hover:text-red-400 block"
-          target="_blank"
-          href="https://github.com/jaarabytes"
-          >you build ?</a>
+      <div className="mt-5 px-[20%]">
+          <Navbar />
+          <Image
+          src="/kurapika.jpg"
+            alt="That's literally me (I'm oozing aura)"
+            width={200}
+            height={200}
+            className="items-center inline my-10"
+            style={{borderRadius: "50%"}}
+            />
+          <h1>Xavier</h1>
+          <p className="mb-5">(yes xavier is my real name)</p>
+          <p className="mb-5">Working on da blockchain</p>
+          <div className="block mb-5">
+            <a
+            className="hover:underline hover:text-red-400 block"
+            target="_blank"
+            href="https://twitter.com/jaarabytes"
+            >bird app ?</a>
+            <a
+            className="hover:underline hover:text-red-400 block pb-3"
+            target="_blank"
+            href="https://github.com/jaarabytes"
+            >you build ?</a>
+            <hr />
+          </div>
+          <AboutPage />
         </div>
-        <div className="flex justify-evenly text-center">
-          {relevantLinks.map((element, index) => (
-            <Link className="text-red-400" href={element.path} target="_self" key={index}>
-              {element.name}
-            </Link>
-          ))}
-        </div>
-      </div>
     </>
   )
 }
