0e63a7b4c9c879b55a58d6b95de0d438c05f3093
Jaarabytes <xavierandole78@gmail.com>
2024-06-12 11:20:37 +0300

    refactor: reduce garbage
    remove excess garbage. its now better. todo: link the thoughts tab with a `notes.md`

--- app/ui/About.tsx
+++ app/ui/About.tsx
@@ -6,29 +6,14 @@ export const metadata : Metadata = {
 }
 
 export default function AboutPage () {
-    const interests = ["infosec (especially web3)","literature", "Attack on titan", "Ludovico Eunadi", "finance", "and The Sopranos"]
-    const music = [{link: "https://open.spotify.com/playlist/4sNiUUrxothKlARvtcbjkU?si=5XeFsXNEREaG8RPfRPEL9A&utm_source=copy-link", name: "storytelling"},
-                    {link: "https://open.spotify.com/playlist/3jhVe6KnUKOccv3um56FP8?si=HWQJEV4jQYSR37UNFOUtBg&utm_source=copy-link", name: "jazz"},
-                    {link: "https://open.spotify.com/playlist/2qXjPCmMuGRlVDRRFFetAs?si=uy6K2tOpSUedQF8dz16elA&utm_source=copy-link", name: "experience vibes"},
-                    {link: "https://open.spotify.com/playlist/4y85MI3qRoui5GPSHWl8Op?si=0og1UeQUQo6fb-9Ds4bMYw&utm_source=copy-link", name: "nostalgia"},
-    ]
-    const creations = [{link : "https://jipime.vercel.app", name: "jipime (IQ test)"}, {link : "https://streamwan.vercel.app", name: "streamwan"}]
-    const relevantLinks = [{link: "https://www.coursera.org/learn/learning-how-to-learn", name: "quick learning"},
-        {link: 'https://www.google.com/search?q=A+Mind+For+Numbers%3A+How+to+Excel+at+Math+and+Science&sca_esv=8ee0a4bf7d87b514&sxsrf=ADLYWIJQGPluYmXbjw0mQdbezpn3lPtgwQ%3A1716282706177&ei=UmVMZuCgCqr2i-gPvMqsgAk&ved=0ahUKEwjgkaSos56GAxUq-wIHHTwlC5AQ4dUDCBA&uact=5&oq=A+Mind+For+Numbers%3A+How+to+Excel+at+Math+and+Science&gs_lp=Egxnd3Mtd2l6LXNlcnAiNEEgTWluZCBGb3IgTnVtYmVyczogSG93IHRvIEV4Y2VsIGF0IE1hdGggYW5kIFNjaWVuY2UyBBAjGCcyBBAjGCcyCxAuGIAEGJECGIoFMgUQABiABDIFEAAYgAQyBBAAGB4yCxAAGIAEGIYDGIoFMgsQABiABBiGAxiKBTIIEAAYgAQYogRI5hRQLVi3CHABeAGQAQCYAdcDoAGHEqoBBzItMy4zLjG4AQPIAQD4AQGYAgOgArcEwgIKEAAYsAMY1gQYR8ICBRAuGIAEmAMAiAYBkAYIkgcFMS4wLjKgB9U6&sclient=gws-wiz-serp', name: 'solving math'},
-        {link : 'https://x.com/yacineMTB/status/1789787186763530449', name:"yacineMTB W"}
-    ]
+    const creations = [{link : "https://jipime.vercel.app", name: "jipime", description: "Ever thought how high IQ you are?"}, 
+    {link : "https://streamwan.vercel.app", name: "streamwan", description: "Wi-Fi connection B2B/B2C service."}]
+    
     return (
         <>
             <h1>About?</h1>
-            <p>Hey, I do frontend, little backend and blockchain</p>
+            <p>Front, back, db, containers & blockchain</p>
             <p>I can fix a printer</p>
-            <p className="my-5">Other interests include {interests.map((elem, index) => 
-            <>
-            <span key={index}>
-                {elem}
-                {index < interests.length - 1 && ', '}
-            </span>
-            </>)}</p>
             <p className="mt-5">If you are reading this, feel free to send me a message! Dm me on <a
             href="https://twitter.com/jaarabytes"
             className="hover: underline hover:text-red-400"
@@ -38,32 +23,12 @@ export default function AboutPage () {
             className="hover: underline hover:text-red-400"
             target="_blank"
             >mail me!</a></p>
-            <p>What i have learned so far is that nothing matters, just do stuff, and if you&apos;re scared do it scared! the regret of inaction is always &gt; that of inaction</p>
-            <p>&ldquo;A ship is safe in harbor, but that&apos;s not what ships are for&ldquo; ~William G.T Shedd</p>
-            <p>Goku faced more and did not give up, what of you?</p>
-            <p className="mt-5">🇰🇪🇰🇪🇰🇪🇰🇪 leads Africa in tech</p>
-            <p className="mt-5">Good music: </p>
-            <div className="mx-3">
-            {music.map((element, index) => (
-                    <dl key={index}>
-                        <li><a href={element.link} className="hover:underline hover:text-red-400" target="_blank">{element.name}</a></li>
-                    </dl>
-                ))}
-            </div>
-            <h1 className="mt-5">My creations: </h1>
+            <h1 className="mt-5">Buildings: </h1>
             <div className="mx-3">
                 {creations.map((element, index) => (
-                    <dl key={index}>
-                        <li><a href={element.link} className="hover:underline hover:text-red-400" target="_blank">{element.name}</a></li>
-                    </dl>
-                ))}
-            </div>
-            {/* Add certifications and resume possibly */}
-            <p className="mt-5">Motivators (relevant links that are to be studied thoroughly): </p>
-            <div className="mx-2">
-                {relevantLinks.map((element, index) => (
-                    <dl key={index}>
-                        <li><a href={element.link} className="hover:underline hover:text-red-400" target="_blank">{element.name}</a></li>
+                    <dl key={index} className="mt-3">
+                        <dt><a href={element.link} className="underline text-red-400" target="_blank">{element.name}</a></dt>
+                        <dd>{element.description}</dd>
                     </dl>
                 ))}
             </div>
