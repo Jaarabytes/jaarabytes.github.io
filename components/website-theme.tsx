a154b601bb227b70179c79b9393e514da105cd02
Jaarabytes <xavierandole78@gmail.com>
2024-06-19 15:22:04 +0300

    feat: add most iconic song EVERRRR
    add fifa 14 theme song together with a fixed play button

---
+++ components/website-theme.tsx
@@ -0,0 +1,32 @@
+import { useState, useRef } from 'react';
+import { FaMusic } from "react-icons/fa6";
+const PlayButton: React.FC = () => {
+  const [playing, setPlaying] = useState(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const togglePlay = () => {
+    if (playing) {
+      audioRef.current?.pause();
+    } else {
+      audioRef.current?.play();
+    }
+    setPlaying(!playing);
+  };
+
+  return (
+    <div className="fixed bottom-4 right-4">
+      <button
+        onClick={togglePlay}
+        className="px-6 py-2 bg-blue-500 text-white rounded text-xs"
+      >
+        <FaMusic className="text-3xl" />
+        {/* 30 seconds is little, make it 50 seconds or 1 minute */}
+        {/* The song is Love me again by John Newman, it's the FIFA 14 Theme song */}
+        <p className='py-2'>{playing ? 'Pause' : 'Play!'}</p>
+      </button>
+      <audio ref={audioRef} src="/real.mp3" />
+    </div>
+  );
+};
+
+export default PlayButton;
