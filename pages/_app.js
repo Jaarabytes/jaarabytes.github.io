2162c4986e4492ec093da145387fe3a042fedd11
Jaarabytes <xavierandole78@gmail.com>
2024-06-19 15:25:30 +0300

    feat: add navbar, website-theme and metadata
    add navbar for navigation, website-theme for greatful anons and metadata
    to help da needy

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "../styles/globals.css"
 import { Inter } from '@next/font/google';
 
@@ -5,8 +6,14 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function MyApp({ Component, pageProps }) {
   return (
+    <>
     <div className={`${inter.className} sm:px-[20%] px-3 my-5 min-h-[100vh]`}>
+      <Head>
+        <title>jaarabytes</title>
+        <link rel="icon" href="https://freepngimg.com/download/artwork/84555-piece-warriors-artwork-one-water-pirate-trafalgar.png" />
+      </Head>
       <Component {...pageProps} />
     </div>
+    </>
   );
 }
