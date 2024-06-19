5e393daa0c233b3d8cce3ba6edb93132671bd712
Jaarabytes <xavierandole78@gmail.com>
2024-06-19 21:47:19 +0300

    feat: impelement pulse animation
    add a pulse animation for the theme song element that pans out
    infinitely

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ const config = {
   ],
   theme: {
     extend: {
+      animation: {
+        'pulse-slow': "pulse-slow 1.5s infinite"
+      },
+      keyframes: {
+        'pulse-slow': {
+          '0%, 100%': {
+            transform: 'scale(1)',
+          },
+          '50%': {
+            transform: 'scale(1.05)'
+          }
+        }
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
