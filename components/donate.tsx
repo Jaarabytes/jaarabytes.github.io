f4691e0ed733ec7c8d8feb95ac80a19709af0c9d
Jaarabytes <xavierandole78@gmail.com>
2024-06-29 11:54:02 +0300

    fix: finalize paystack
    fixed all pf paystack and its related errors. It actually works. Thank
    you all.

--- components/donate.tsx
+++ components/donate.tsx
@@ -1,26 +1,32 @@
 // Paystack component that accepts donations
 //
-import { usePaystackPayment } from 'react-paystack';
+import { usePaystackPayment, PaystackButton } from 'react-paystack';
 import { useState } from 'react';
 export default function Donate () {
     const [ amount, setAmount ] = useState(0);
-    const config = { reference: (new Date()).getTime().toString(), email: "xh3rking96@gmail.com", amount: amount, publicKey: 'pk_live_9999378c83331abc0c642f96d2457a5f88969934' }
+    const config = {
+        reference: (new Date()).getTime().toString(),
+        email: "xh3rking96@gmail.com", 
+        amount: amount,
+        publicKey: 'pk_live_9999378c83331abc0c642f96d2457a5f88969934',
+        currency: 'KES'
+        }
     const onSuccess = ( reference: any ) => { console.log(reference) }
     const onClose = () => { console.log("closed") }
-    const initializePayment = usePaystackPayment(config);
     return (
             <>
                 <div className='text-center my-5' style={{minHeight: '100vh'}}>  
                     <h1 className='text-2xl font-bold my-5'>Thank you for donating anon!</h1>
                     <label>Amount</label>
                     <br /><br />
-                    <input className='text-center rounded-sm text-black py-3' value={amount} onChange={() => setAmount(Number(event.target.value))} />
+                    <input className='text-center rounded-sm text-black py-3' value={amount} onChange={(e) => setAmount(Number(e.target.value))} />
                     <br /><br />
-                    <button onClick={() => initializePayment(onSuccess, onClose)}
+                    <PaystackButton
                     className='my-5 py-2 text-white rounded-lg px-3 bg-green-900 hover:bg-green-700'
-                    >
-                        Money upvote
-                    </button>
+                    onSuccess={onSuccess} onClose={onClose}
+                    text='Money Upvote'
+                    {...config}
+                    />
                 </div>
            </>
            )
